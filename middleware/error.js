3380e2ca13b263b9372eac60c24fff3b145844e9
Jordi <jordifv90@gmail.com>
2024-03-19 11:36:08 +0100

    duplicated keys and validation error

--- middleware/error.js
+++ middleware/error.js
@@ -5,13 +5,27 @@ const errorHandler = (err, req, res, next) => {
     error.message = err.message;
 
     // Log to console for the developer
-    console.log(err.stack.red);
+    console.log(err);
 
     // Mongoose bad ObjectID
     if (err.name === "CastError") {
         const message = `Bootcamp not found with id of ${err.value}`
         const statusCode = 404
         error = new ErrorResponse(message,statusCode)
+    }
+
+    // Duplicated key
+    if (err.code === 11000){
+        const message = `Duplicated value ${Object.values(err.keyValue)}`
+        const statusCode = 400
+        error = new ErrorResponse(message,statusCode)
+    }
+
+    // Validation errors
+    if( err.name === "ValidationError"){
+        const message = Object.values(err.errors).map(value => value.message)
+        const statusCode = 400
+        error = new ErrorResponse(message, statusCode)
     }
 
     res.status(error.statusCode || 500).json({success: false, error: error.message || "Server error"})
