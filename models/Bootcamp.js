1683512ec9e131cee8519b0aecbae98187d998dc
Jordi <jordifv90@gmail.com>
2024-03-22 11:59:46 +0100

    Populate, virtual and cascade on delete courses -> bootcamp

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -98,6 +98,9 @@ const BootcampSchema = new mongoose.Schema({
         type: Date,
         default: Date.now,
     },
+}, {
+    toJSON: {virtuals: true},
+    toObject: {virtuals: true}
 });
 
 // Create bootcamp slug from the name
@@ -107,7 +110,7 @@ BootcampSchema.pre("save", function (next) {
 })
 
 // Geocoder & create location field
-BootcampSchema.pre("save",async function (next) {
+BootcampSchema.pre("save", async function (next) {
     const loc = await geocoder.geocode(this.address)
     this.location = {
         type: "Point",
@@ -124,6 +127,22 @@ BootcampSchema.pre("save",async function (next) {
     this.address = undefined;
 
     next()
+})
+
+// Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre("deleteOne", {document: true, query: false},
+    async function (next) {
+        console.log(`Courses being removed from bootcamp ${this._id}`);
+        await this.model("Course").deleteMany({bootcamp: this._id});
+        next()
+    })
+
+// Reverse populate with virtuals
+BootcampSchema.virtual("courses", {
+    ref: "Course",
+    localField: "_id",
+    foreignField: "bootcamp",
+    justOne: false
 })
 
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
