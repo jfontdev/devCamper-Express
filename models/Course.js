68afd5b34f42f72a622b9e1d1b4c554226d9ea97
Jordi <jordifv90@gmail.com>
2024-03-22 14:51:13 +0100

    Add averageCost

--- models/Course.js
+++ models/Course.js
@@ -1,7 +1,7 @@
-const mongoose= require("mongoose")
+const mongoose = require("mongoose")
 
 const CourseSchema = new mongoose.Schema({
-    title : {
+    title: {
         type: String,
         trim: true,
         required: [true, "Please add a course title"],
@@ -18,11 +18,11 @@ const CourseSchema = new mongoose.Schema({
         type: Number,
         required: [true, "Please add a tuition cost"]
     },
-    minimumSkill:{
+    minimumSkill: {
         type: String,
         required: [true, "Please add a minimum skill"],
-        enum: ["beginner","intermediate","advanced"]
-    },scholarshipAvailable: {
+        enum: ["beginner", "intermediate", "advanced"]
+    }, scholarshipAvailable: {
         type: Boolean,
         default: false
     },
@@ -30,11 +30,46 @@ const CourseSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     },
-    bootcamp:{
+    bootcamp: {
         type: mongoose.Schema.ObjectId,
         ref: 'Bootcamp',
         required: true
     }
+})
+
+// Static method to get avg of course tuitions
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+    console.log("Calculating avg cost...".blue)
+
+    const obj = await this.aggregate([
+        {
+            $match: {bootcamp: bootcampId}
+        },
+        {
+            $group: {
+                _id: "$bootcamp",
+                averageCost: {$avg: "$tuition"}
+            }
+        }
+    ])
+
+    try {
+        await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+            averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+        })
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+// Call getAverageCost after save
+CourseSchema.post("save", function () {
+    this.constructor.getAverageCost(this.bootcamp)
+})
+
+// Call getAverageCost before remove
+CourseSchema.pre("deleteOne", {document: true, query: false}, function () {
+    this.constructor.getAverageCost(this.bootcamp)
 })
 
 module.exports = mongoose.model("Course", CourseSchema);
