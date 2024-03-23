4d3006c6175e723750e18a02684d253806466cab
Jordi <jordifv90@gmail.com>
2024-03-23 12:29:23 +0100

    Auth route, User model, jwt-bcrypt

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ connectDB();
 // Route files
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses")
+const auth = require("./routes/auth")
 const path = require("path");
 
 // Initialize express server with port and node enviroment
@@ -38,6 +39,7 @@ app.use(express.static(path.join(__dirname, "public")))
 // Mount routers -> middleware
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
 
 app.use(errorHandler);
 
