c91958d2371db03f78e821b3d50ede16edd33c50
Jordi <jordifv90@gmail.com>
2024-03-23 16:18:22 +0100

    Add user to bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -29,7 +29,20 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @route   POST /api/v1/bootcamps/
 // @access  Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+    // Add user to body
+    req.body.user = req.user.id;
+
+
+    // Check for published bootcamp
+    const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id })
+
+    // Check user role, publisher can add only one
+    if (publishedBootcamp && req.user.role !== "admin"){
+        return next(new ErrorResponse(`The user with ID ${req.user.id} has already published a bootcamp`, 400))
+    }
+
     const bootcamp = await Bootcamp.create(req.body);
+
     res.status(201).json({success: true, data: bootcamp});
 });
 
