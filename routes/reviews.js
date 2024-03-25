ffa5319274bdf56eb519ec0cda119af5fef81565
Jordi <jordifv90@gmail.com>
2024-03-25 15:24:17 +0100

    Add Review model, and getReviews route

---
+++ routes/reviews.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const {
+    getReviews
+} = require("../controllers/reviews");
+
+const Review = require("../models/Review")
+const advancedResults = require("../middleware/advancedResults")
+
+const router = express.Router({mergeParams: true});
+
+const {protect, authorize} = require("../middleware/auth")
+
+// Get all reviews
+router.route("/").get(
+    advancedResults(Review, {
+        path: "bootcamp",
+        select: "name description"
+    }), getReviews
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -17,6 +17,7 @@ connectDB();
 // Route files
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses")
+const reviews = require("./routes/reviews")
 const auth = require("./routes/auth")
 const users = require("./routes/users")
 const path = require("path");
@@ -44,6 +45,7 @@ app.use(express.static(path.join(__dirname, "public")))
 // Mount routers -> middleware
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
+app.use("/api/v1/reviews", reviews);
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/users", users);
 
