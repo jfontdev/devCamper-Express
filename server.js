13171df3385db9a3f1fb70218c88b50e0c8ebe84
Jordi <jordifv90@gmail.com>
2024-03-26 11:25:58 +0100

    Add rate limiter, hpp and cors

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const morgan = require("morgan");
 const mongoSanitize = require("express-mongo-sanitize")
 const helmet = require("helmet")
 const xss = require("xss-clean")
+const rateLimit = require("express-rate-limit")
+const hpp = require("hpp")
+const cors = require("cors")
 const colors = require("colors");
 const fileupload = require("express-fileupload")
 const cookieParser = require("cookie-parser")
@@ -46,6 +49,20 @@ app.use(helmet());
 
 // Prevent XSS tags
 app.use(xss())
+
+// Rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 mins
+    max: 100
+})
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp())
+
+// Enable cors
+app.use(cors())
 
 // File uploading
 app.use(fileupload());
